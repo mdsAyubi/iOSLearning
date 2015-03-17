40e1f8d3b343837df0b41469bfbf4e65e3edcf0d
mdsAyubi <mds.ayubi@gmail.com>
2015-03-18 04:58:26 +0530

    Files for Where Am I App

---
+++ Where Am I/Where Am I/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  Where Am I
+//
+//  Created by ayubi on 18/03/15.
+//  Copyright (c) 2015 Gazelle. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var manager:CLLocationManager!
+    
+    
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+    
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        println(locations[0])
+        
+        var userLocation:CLLocation = locations[0] as CLLocation
+        self.latitudeLabel.text = "\(userLocation.coordinate.latitude)"
+        self.longitudeLabel.text = "\(userLocation.coordinate.longitude)"
+        
+        self.courseLabel.text = "\(userLocation.course)"
+        self.speedLabel.text = "\(userLocation.speed)"
+        self.altitudeLabel.text = "\(userLocation.altitude)"
+        
+        //CLGeocoder().reverseGeocoderLocation(userLocation, completionHandler:{(placemarks,error)->Void in
+        
+        CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler: { (placemarks, error) -> Void in
+            
+            if (error != nil){
+                println(error)
+            } else{
+                if let p = CLPlacemark(placemark: placemarks?[0] as CLPlacemark){
+                    println(p)
+                    self.addressLabel.text = "\(p.locality), \(p.country)"
+                }
+                else{
+                    println("Nothing...no error no value")
+                }
+            }
+        
+        
+        })
+        
+        
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

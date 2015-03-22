2a3bdf5c9a04c619b95c2a4d4b855e91c0f0d9d4
mdsAyubi <mds.ayubi@gmail.com>
2015-03-23 00:58:48 +0530

    Files for Advanced Segues

---
+++ Advanced Segues/Advanced Segues/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Advanced Segues
+//
+//  Created by ayubi on 20/03/15.
+//  Copyright (c) 2015 Gazelle. All rights reserved.
+//
+
+import UIKit
+var rowCounter:Int = 0
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println(rowCounter)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

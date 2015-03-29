a2a8ca32f0cc09abd4dccc891c4ea1627ec8051b
mdsAyubi <mds.ayubi@gmail.com>
2015-03-30 03:46:44 +0530

    Add files for random hilarious sound player

--- ShakesAndSwipes/ShakesAndSwipes/ViewController.swift
+++ ShakesAndSwipes/ShakesAndSwipes/ViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player :AVAudioPlayer = AVAudioPlayer()
+    var sounds = ["bean","belch","giggle","pew","pig","snore","static","uuu"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,8 +71,20 @@ class ViewController: UIViewController {
     override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
         
         if event.subtype == UIEventSubtype.MotionShake {
-            
             println("User shakes...")
+            
+            var randomNumber = Int(arc4random_uniform(UInt32(sounds.count)))
+            
+            var fileLocation = NSBundle.mainBundle().pathForResource(sounds[randomNumber], ofType: "mp3")
+            var error:NSError? = nil
+            
+            player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: fileLocation!), error: &error)
+            player.play()
+            
+            
+            
+            
+            
         }
     }
     

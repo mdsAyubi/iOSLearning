d96d612ba5eb43ce0d6fb1b9f818a4f214e2128c
mdsAyubi <mds.ayubi@gmail.com>
2015-07-01 04:25:40 +0530

    Parse Learning| View users feed of photos

---
+++ parse-starter-project-1/ParseSwiftStarterProject/ParseStarterProject/Cell.swift
@@ -0,0 +1,19 @@
+//
+//  Cell.swift
+//  ParseStarterProject
+//
+//  Created by ayubi on 01/07/15.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+
+    @IBOutlet weak var postedImage: UIImageView!
+   
+    @IBOutlet weak var userName: UILabel!
+    
+    @IBOutlet weak var message: UILabel!
+    
+}
